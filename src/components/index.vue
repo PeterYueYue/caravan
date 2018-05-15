8c71ac23347422d956030f74834807de655bee15
miaojing <1336619526@qq.com>
2018-05-15 17:02:11 +0800

    1

--- src/components/index.vue
+++ src/components/index.vue
@@ -120,7 +120,7 @@
       }
     },
     mounted() {
-      this.wx.config({
+      wx.config({
         // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         debug: false,
         // 必填，公众号的唯一标识
@@ -133,11 +133,11 @@
         jsApiList: ['checkJsApi', 'scanQRCode']
       });
 
-      this.wx.error(function (res) {
+      wx.error(function (res) {
         alert("出错了：" + res.errMsg);//这个地方的好处就是wx.config配置错误，会弹出窗口哪里错误，然后根据微信文档查询即可。
       });
 
-      this.wx.checkJsApi({
+      wx.checkJsApi({
         jsApiList: ['scanQRCode'],
         success: function (res) {
         }
@@ -276,7 +276,7 @@
         });
       },
       scanQRCode() {
-        this.wx.scanQRCode({
+        wx.scanQRCode({
           needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function (res) {

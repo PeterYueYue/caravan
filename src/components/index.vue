7fa6c0bba72427c05b18b8481dadea252a93e02f
miaojing <1336619526@qq.com>
2018-05-17 17:11:39 +0800

    1

--- src/components/index.vue
+++ src/components/index.vue
@@ -129,6 +129,26 @@
       }
     },
     mounted() {
+      this.$http.get(this.$HOST + '/openapi/v2/app/hm/getWeixinConf').then((res) => {
+        const sign = res.data.content
+        wx.config({
+          // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false,
+          // 必填，公众号的唯一标识
+          appId: 'wxc3ef5ae7650dbc82',
+          timestamp: sign.timestamp,
+          nonceStr: sign.nonceStr,
+          signature: sign.signature,
+
+          // 必填，需要使用的JS接口列表
+          jsApiList: ['scanQRCode']
+        });
+        wx.error(function (res) {
+          alert("出错了：" + res.errMsg);//这个地方的好处就是wx.config配置错误，会弹出窗口哪里错误，然后根据微信文档查询即可。
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
     },
     methods: {
       submitBtn() {
@@ -298,33 +318,10 @@
           });
         }
         if (env.indexOf('MicroMessenger') > -1) {
-          alert(1);
           this.wxConfig();
         }
       },
       wxConfig() {
-        alert(2);
-        this.$http.get(this.$HOST + '/openapi/v2/app/hm/getWeixinConf').then((res) => {
-          const sign = res.data.content;
-          wx.config({
-            // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            debug: false,
-            // 必填，公众号的唯一标识
-            appId: 'wxc3ef5ae7650dbc82',
-            timestamp: sign.timestamp,
-            nonceStr: sign.nonceStr,
-            signature: sign.signature,
-
-            // 必填，需要使用的JS接口列表
-            jsApiList: ['scanQRCode']
-          });
-          wx.error(function (res) {
-            alert("出错了：" + res.errMsg);//这个地方的好处就是wx.config配置错误，会弹出窗口哪里错误，然后根据微信文档查询即可。
-          });
-        }).catch((error) => {
-          console.log(error);
-        });
-        alert(3);
         this.scanQRCode();
       },
       scanQRCode() {

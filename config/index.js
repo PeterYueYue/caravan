96777adff003484f010a393703257a00588ff1ff
miaojing <1336619526@qq.com>
2018-07-17 13:17:14 +0800

    1

--- config/index.js
+++ config/index.js
@@ -31,8 +31,8 @@ module.exports = {
     proxyTable: {
       '/openapi': { //这里是我配置的名字
         // 正式
-        target: 'https://app.greenfortune.sh.cn',
-        // target: 'http://180.153.19.162:8082',
+        // target: 'https://app.greenfortune.sh.cn',
+        target: 'http://47.90.121.211',
 
         changeOrigin: true,
         pathRewrite: {

ebeb71e9ee4984042c8da800881b8f9a354ed604
miaojing <1336619526@qq.com>
2019-06-03 10:39:52 +0800

    getWeixinConf接口地址加参数districtName

--- src/components/index.vue
+++ src/components/index.vue
@@ -143,7 +143,7 @@
     mounted() {
       let env = window.navigator.userAgent;
       if (env.indexOf('MicroMessenger') > -1) {
-        this.$http.get(this.$HOST + '/openapi/v2/app/hm/getWeixinConf').then((res) => {
+        this.$http.get(this.$HOST + '/openapi/v2/app/hm/getWeixinConf'+'?'+'districtName='+decodeURIComponent(this.$route.query.districtName)).then((res) => {
           const sign = res.data.content
           wx.config({
             // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
